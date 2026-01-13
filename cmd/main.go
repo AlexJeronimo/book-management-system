f189c7fa4a3927c30d75fa7dc775e0a2d80b61ce
Alex Jeronimo <jeronimoalex@gmail.com>
2026-01-13 10:05:47 +0000

    feat: add Load test in main

--- cmd/main.go
+++ cmd/main.go
@@ -17,19 +17,25 @@ func main() {
 	repo := book.Repository{}
 
 	id := repo.GenerateID()
-	book := book.NewBook("title", []string{"author"}, " ISBN", true, id)
-	fmt.Printf("%+v\n", book)
-	data, err := json.MarshalIndent(book, "", " ")
+	book1 := book.NewBook("title", []string{"author"}, "ISBN", true, id)
+	fmt.Printf("%+v\n", book1)
+	data, err := json.MarshalIndent(book1, "", " ")
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(string(data))
 
-	repo.Add(book)
+	repo.Add(book1)
 
 	err = repo.Save(bookshelfFile)
 	if err != nil {
 		fmt.Println("can't save data to json file. Error: ", err)
 	}
+
+	newRepo := book.NewRepository()
+
+	newRepo.Load(bookshelfFile)
+
+	fmt.Printf("%+v\n", newRepo)
 
 }
